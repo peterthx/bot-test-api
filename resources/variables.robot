3d7fa1af59360186c9b06405cc4cf9d0b1509f34
theerapat.demo <theerapat.demo@gmail.com>
2025-08-16 15:12:07 +0700

    up ci.yml with terraform

--- resources/variables.robot
+++ resources/variables.robot
@@ -1,4 +1,6 @@
 *** Variables ***
-${BASE_URL}       http://localhost:3000
-# ${AUTH_TOKEN}     Bearer 1234567890abcdef
+${BASE_URL}       %{BASE_URL=http://localhost:8080}
 ${CONTENT_TYPE}   application/json
+# ${BASE_URL}       http://localhost:3000
+# ${AUTH_TOKEN}     Bearer 1234567890abcdef
+
---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "httpbin" {
+  name         = "kennethreitz/httpbin"
+  keep_locally = false
+}
+
+resource "docker_container" "httpbin" {
+  name  = "httpbin"
+  image = docker_image.httpbin.latest
+  ports {
+    internal = 80
+    external = 8080
+  }
+  # healthcheck แบบง่าย ๆ
+  healthcheck {
+    test     = ["CMD", "wget", "-q", "-O", "-", "http://localhost/headers"]
+    interval = "10s"
+    timeout  = "2s"
+    retries  = 3
+  }
+}
+
+output "base_url" {
+  value = "http://localhost:8080"
+}
