b4166a211a98ecb6f401e71652e97cfdbe8d1380
theerapat.demo <theerapat.demo@gmail.com>
2025-08-17 13:36:24 +0700

    update & change path project

--- regression-test.sh
+++ regression-test.sh
@@ -1,4 +1,4 @@
 for i in {1..10}; do
     echo "Run to round #$i time(s)"
-    robot --outputdir tests/results/$(date +%Y%m%d_%H%M%S) tests/health_check.robot
+    robot --outputdir tests/suites/results/$(date +%Y%m%d_%H%M%S) tests/suites/health_check.robot
 done
